037635d13f38e892849181be2c489ae795fd5f59
kahshiu-ipp <kahshiu.chong@iproperty.com.my>
2025-02-01 08:32:13 +0800

    setup PageWithRef component

---
+++ src/lib/components/PageWithRef.svelte
@@ -0,0 +1,24 @@
+<script>
+	let { target, ref } = $props();
+	let isPinned = $state(false);
+</script>
+
+<div class="content">
+	<div class="content-target">
+		{#if target}
+			{@render target?.()}
+		{:else}
+			Target content here
+		{/if}
+	</div>
+	<div class="content-ref {isPinned ? 'hello' : 'bellow'}">
+		<button onclick={() => (isPinned = !isPinned)}>Toggle</button>
+		<div>
+			{#if target}
+				{@render ref?.()}
+			{:else}
+				Drawing reference here
+			{/if}
+		</div>
+	</div>
+</div>
